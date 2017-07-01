dbc04cc130ec180fc99e8d7d7348ec364a1493cf
Meathill <meathill@gmail.com>
2017-07-01 18:14:42 +0800

    基本完成

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,21 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-var header = require('gulp-header');
+var stylus = require('gulp-stylus');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 var pkg = require('./package.json');
 
-// Set the banner content
-var banner = ['/*!\n',
-    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
-    ' */\n',
-    ''
-].join('');
-
 // Compiles SCSS files from /styl into /css
-gulp.task('sass', function() {
-    return gulp.src('styl/agency.stylus')
-        .pipe(sass())
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+gulp.task('stylus', function() {
+  return gulp.src('styl/agency.stylus')
+    .pipe(stylus({
+      'include css': true
+    }))
+    .pipe(cleanCSS({
+      level: 2
+    }))
+    .pipe(rename('agency.min.css'))
+    .pipe(gulp.dest('css'))
 });
 
 // Minify compiled CSS
